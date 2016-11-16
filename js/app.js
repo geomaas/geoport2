28b86102c85e6f97ac3d210f8c9a3ff8135357ff
geomaas <geoff.maas@gmail.com>
2016-11-16 13:26:32 -0500

    some new layout ideas

---
+++ js/app.js
@@ -0,0 +1,2 @@
+
+TweenMax.to("#bike", 1, {opacity:0, scale: 0, ease: Bounce.easeOut});
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
+TweenMax.to("#bike", 1, {opacity:0, scale: 0, ease: Bounce.easeOut});
+
 },{}]},{},[1])
