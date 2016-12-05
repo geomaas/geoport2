33de9ed5db025c1f246c1c62922b278abcbe6137
geomaas <geoff.maas@gmail.com>
2016-12-05 16:32:14 -0500

    adding media queries and fixing contact section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
-// var controller = new ScrollMagic.Controller();
-//
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: "#geoff"
-//     })
-//     .setTween("#geoff-pic", 0.5, {
-//         scale: 1.5
-//     })
-//     .addTo(controller);
+$(document).ready(function() {
+    // var controller = new ScrollMagic.Controller();
+    //
+    // var scene = new ScrollMagic.Scene({
+    //   triggerElement: "#geoff"
+    //     })
+    //     .setTween("#geoff-pic", 0.5, {
+    //         scale: 1.5
+    //     })
+    //     .addTo(controller);
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
+    $('a[href^="#"]').on('click',function (e) {
+    	    e.preventDefault();
+
+    	    var target = this.hash;
+    	    var $target = $(target);
+
+    	    $('html, body').stop().animate({
+    	        'scrollTop': $target.offset().top
+    	    }, 900, 'swing', function () {
+    	        window.location.hash = target;
+    	    });
+    	});
+
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,31 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// var controller = new ScrollMagic.Controller();
-//
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: "#geoff"
-//     })
-//     .setTween("#geoff-pic", 0.5, {
-//         scale: 1.5
-//     })
-//     .addTo(controller);
+$(document).ready(function() {
+    // var controller = new ScrollMagic.Controller();
+    //
+    // var scene = new ScrollMagic.Scene({
+    //   triggerElement: "#geoff"
+    //     })
+    //     .setTween("#geoff-pic", 0.5, {
+    //         scale: 1.5
+    //     })
+    //     .addTo(controller);
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
+    $('a[href^="#"]').on('click',function (e) {
+    	    e.preventDefault();
+
+    	    var target = this.hash;
+    	    var $target = $(target);
+
+    	    $('html, body').stop().animate({
+    	        'scrollTop': $target.offset().top
+    	    }, 900, 'swing', function () {
+    	        window.location.hash = target;
+    	    });
+    	});
+
+});
 
 },{}]},{},[1])
