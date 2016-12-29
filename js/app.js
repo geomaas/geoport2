e51c36ca34c321c26e4150730b452217e4e9c394
geomaas <geoff.maas@gmail.com>
2016-12-28 19:10:21 -0500

    added swipebox to interests

--- js/app.js
+++ js/app.js
@@ -22,21 +22,81 @@ $(document).ready(function() {
 
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
-        }, 900, 'swing', function() {
+        }, 800, 'swing', function() {
             window.location.hash = target;
         });
     });
 
     /*---------------- Hobby Window click events ------------------*/
-    $('.hobby-container').hide();
+    // $('.hobby-container').hide();
 
-    $('.hobby').click(function(e) {
-        e.preventDefault();
-        $('#hobby-win').slideToggle();
-    });
+    // $('.hobby').click(function(e) {
+    //     e.preventDefault();
+    //     $('#hobby-win').slideToggle();
+    // });
+    //
+    // $('#close-hobby').click(function(e) {
+    //     e.preventDefault();
+    //     $('#hobby-win').slideUp();
+    // });
 
-    $('#close-hobby').click(function(e) {
-      e.preventDefault();
-      $('#hobby-win').slideUp();
-    });
+    /*----------------- swipebox -----------------------------*/
+//theatre
+    $( '.theatre' ).click( function( e ) {
+	     e.preventDefault();
+	      $.swipebox( [
+		        { href:'assets/setphotos/jimmydean1.jpg', title:'My Caption' },
+		        { href:'assets/setphotos/jimmydean2.jpg', title:'My Second Caption' },
+            { href:'assets/setphotos/lovesfire1.jpg', title:'third caption'},
+            { href:'assets/setphotos/lovesfire2.jpg', title:''},
+            { href:'assets/setphotos/desdemona1.jpg', title:''},
+            { href:'assets/setphotos/desdemona2.jpg', title:''},
+            { href:'assets/setphotos/blackwhite1.jpg', title:''},
+            { href:'assets/setphotos/blackwhite2.jpg', title:''},
+	                ] );
+      });
+
+      $( '.travel' ).click( function( e ) {
+         e.preventDefault();
+          $.swipebox( [
+              { href:'https://www.youtube.com/watch?v=xP4tsbZ17LE', title:''},
+              { href:'https://www.youtube.com/watch?v=14YreqFamtY', title:''},
+                    ] );
+      });
+
+      $( '.camera' ).click( function( e ) {
+         e.preventDefault();
+          $.swipebox( [
+              { href:'assets/photographs/taj_mahal.jpg', title:''},
+              { href:'assets/photographs/bridge_lightning.jpg', title:''},
+              { href:'assets/photographs/bird_house.jpg', title:''},
+              { href:'assets/photographs/blue_angels.jpg', title:''},
+              { href:'assets/photographs/bus.jpg', title:''},
+              { href:'assets/photographs/camel.jpg', title:''},
+              { href:'assets/photographs/cliff.jpg', title:''},
+              { href:'assets/photographs/columns.jpg', title:''},
+              { href:'assets/photographs/den_subway.jpg', title:''},
+              { href:'assets/photographs/fjord.jpg', title:''},
+              { href:'assets/photographs/heart_d_l.jpg', title:''},
+              { href:'assets/photographs/pina_d_l.jpg', title:''},
+              { href:'assets/photographs/roof.jpg', title:''},
+              { href:'assets/photographs/rugs.jpg', title:''},
+
+                    ] );
+      });
+
+      $( '.drone' ).click( function( e ) {
+         e.preventDefault();
+          $.swipebox( [
+              { href:'assets/drone_gifs/2nd_pres.gif', title:''},
+              { href:'assets/drone_gifs/boat_bridge.gif', title:''},
+              { href:'assets/drone_gifs/peninsula.gif', title:''},
+              { href:'assets/drone_gifs/carnival.gif', title:''},
+              { href:'assets/drone_gifs/castle_pinckney.gif', title:''},
+
+                    ] );
+      });
+
+
+    /*---------------- end of jquery ------------------*/
 });
