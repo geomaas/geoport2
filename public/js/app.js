5d1b1788d392eb78b4a386d39efa1d6b81b08cca
geomaas <geoff.maas@gmail.com>
2017-01-03 15:41:17 -0500

    minor text changes

--- public/js/app.js
+++ public/js/app.js
@@ -29,74 +29,148 @@ $(document).ready(function() {
     });
 
     /*---------------- Hobby Window click events ------------------*/
-    // $('.hobby-container').hide();
 
-    // $('.hobby').click(function(e) {
-    //     e.preventDefault();
-    //     $('#hobby-win').slideToggle();
-    // });
-    //
-    // $('#close-hobby').click(function(e) {
-    //     e.preventDefault();
-    //     $('#hobby-win').slideUp();
-    // });
+    $('.hobby-container, .camera-desc, .theatre-desc, .drone-desc, .travel-desc').hide();
+
+    $('.camera').click(function(e) {
+        e.preventDefault();
+        $('.hobby-container, .camera-desc').slideDown();
+        $('.theatre-desc, .drone-desc, .travel-desc').slideUp();
+    });
+
+    $('.theatre').click(function(e) {
+        e.preventDefault();
+        $('.hobby-container, .theatre-desc').slideDown();
+        $('.camera-desc, .drone-desc, .travel-desc').slideUp();
+
+    });
+
+    $('.drone').click(function(e) {
+        e.preventDefault();
+        $('.hobby-container, .drone-desc').slideDown();
+        $('.theatre-desc, .camera-desc, .travel-desc').slideUp();
+    });
+
+    $('.travel').click(function(e) {
+        e.preventDefault();
+        $('.hobby-container, .travel-desc').slideDown();
+        $('.theatre-desc, .drone-desc, .camera-desc').slideUp();
+    });
+
+    $('#close-hobby').click(function(e) {
+        e.preventDefault();
+        $('.hobby-container').slideUp();
+    });
 
     /*----------------- swipebox -----------------------------*/
-//theatre
-    $( '.theatre' ).click( function( e ) {
-	     e.preventDefault();
-	      $.swipebox( [
-		        { href:'assets/setphotos/jimmydean1.jpg', title:'My Caption' },
-		        { href:'assets/setphotos/jimmydean2.jpg', title:'My Second Caption' },
-            { href:'assets/setphotos/lovesfire1.jpg', title:'third caption'},
-            { href:'assets/setphotos/lovesfire2.jpg', title:''},
-            { href:'assets/setphotos/desdemona1.jpg', title:''},
-            { href:'assets/setphotos/desdemona2.jpg', title:''},
-            { href:'assets/setphotos/blackwhite1.jpg', title:''},
-            { href:'assets/setphotos/blackwhite2.jpg', title:''},
-	                ] );
-      });
 
-      $( '.travel' ).click( function( e ) {
-         e.preventDefault();
-          $.swipebox( [
-              { href:'https://www.youtube.com/watch?v=xP4tsbZ17LE', title:''},
-              { href:'https://www.youtube.com/watch?v=14YreqFamtY', title:''},
-                    ] );
-      });
+    $('.swipebox').swipebox({
+        removeBarsOnMobile: false,
+        hideBarsDelay: 5000,
+    });
 
-      $( '.camera' ).click( function( e ) {
-         e.preventDefault();
-          $.swipebox( [
-              { href:'assets/photographs/taj_mahal.jpg', title:''},
-              { href:'assets/photographs/bridge_lightning.jpg', title:''},
-              { href:'assets/photographs/bird_house.jpg', title:''},
-              { href:'assets/photographs/blue_angels.jpg', title:''},
-              { href:'assets/photographs/bus.jpg', title:''},
-              { href:'assets/photographs/camel.jpg', title:''},
-              { href:'assets/photographs/cliff.jpg', title:''},
-              { href:'assets/photographs/columns.jpg', title:''},
-              { href:'assets/photographs/den_subway.jpg', title:''},
-              { href:'assets/photographs/fjord.jpg', title:''},
-              { href:'assets/photographs/heart_d_l.jpg', title:''},
-              { href:'assets/photographs/pina_d_l.jpg', title:''},
-              { href:'assets/photographs/roof.jpg', title:''},
-              { href:'assets/photographs/rugs.jpg', title:''},
+    $('#theatre-gallery').click(function(e) {
+        e.preventDefault();
+        $.swipebox([{
+            href: 'assets/setphotos/jimmydean1.jpg',
+            title: 'Set design for: Come Back to the Five and Dime, Jimmy Dean, Jimmy Dean'
+        }, {
+            href: 'assets/setphotos/jimmydean2.jpg',
+            title: ''
+        }, {
+            href: 'assets/setphotos/lovesfire1.jpg',
+            title: 'Love&#8217;s Fire. Seven short plays inspired by Shakespear&#8217;s sonnets'
+        }, {
+            href: 'assets/setphotos/lovesfire2.jpg',
+            title: ''
+        }, {
+            href: 'assets/setphotos/desdemona1.jpg',
+            title: 'Set of Desdemona: a Play about a Handkerchief'
+        }, {
+            href: 'assets/setphotos/desdemona2.jpg',
+            title: ''
+        }, {
+            href: 'assets/setphotos/blackwhite1.jpg',
+            title: 'Wedding Band: A Love/Hate Story in Black and White'
+        }, {
+            href: 'assets/setphotos/blackwhite2.jpg',
+            title: ''
+        }, ]);
+    });
 
-                    ] );
-      });
+    $('#travel-gallery').click(function(e) {
+        e.preventDefault();
+        $.swipebox([{
+            href: 'https://www.youtube.com/watch?v=xP4tsbZ17LE',
+            title: 'A short video of my travels to Norway with my brother (some slight nsfw language)'
+        }, {
+            href: 'https://www.youtube.com/watch?v=14YreqFamtY',
+            title: 'Another video of my trip to India with my sister'
+        }, ]);
+    });
 
-      $( '.drone' ).click( function( e ) {
-         e.preventDefault();
-          $.swipebox( [
-              { href:'assets/drone_gifs/2nd_pres.gif', title:''},
-              { href:'assets/drone_gifs/boat_bridge.gif', title:''},
-              { href:'assets/drone_gifs/peninsula.gif', title:''},
-              { href:'assets/drone_gifs/carnival.gif', title:''},
-              { href:'assets/drone_gifs/castle_pinckney.gif', title:''},
+    $('#camera-gallery').click(function(e) {
+        e.preventDefault();
+        $.swipebox([{
+                href: 'assets/photographs/taj_mahal.jpg',
+                title: 'Taj Mahal'
+            }, {
+                href: 'assets/photographs/bridge_lightning.jpg',
+                title: 'Lightning over the Cooper'
+            }, {
+                href: 'assets/photographs/bird_house.jpg',
+                title: 'Pigeon House in Doha, Qatar'
+            }, {
+                href: 'assets/photographs/blue_angels.jpg',
+                title: 'Blue Angels in Charleston'
+            }, {
+                href: 'assets/photographs/bus.jpg',
+                title: 'Indian Freight Truck'
+            }, {
+                href: 'assets/photographs/pina_d_l.jpg',
+                title: 'The Pineapple Fountain'
+            }, {
+                href: 'assets/photographs/camel.jpg',
+                title: 'Camel Jockey Doha, Qatar'
+            }, {
+                href: 'assets/photographs/cliff.jpg',
+                title: 'Boy cliff diving in Bermuda'
+            }, {
+                href: 'assets/photographs/columns.jpg',
+                title: 'The Summer Palace St. Petersburg, Russia'
+            }, {
+                href: 'assets/photographs/den_subway.jpg',
+                title: 'Copenhagen Subway Terminal'
+            }, {
+                href: 'assets/photographs/fjord.jpg',
+                title: 'The Sognefjord, Norway'
+            }, {
+                href: 'assets/photographs/elephant.jpg',
+                title: 'Elephant Rider Jaipur, India'
+            }, {
+                href: 'assets/photographs/heart_d_l.jpg',
+                title: 'Gate at St. John&#8217;s reformed Episcopal church'
+            }, {
+                href: 'assets/photographs/roof.jpg',
+                title: 'Ceiling of Museum of Arabic Art Doha, Qatar'
+            }, {
+                href: 'assets/photographs/rugs.jpg',
+                title: 'Jama Masjid Mosque New Delhi, India'
+            },
 
-                    ] );
-      });
+        ]);
+    });
+
+    $('#drone-gallery').click(function(e) {
+        e.preventDefault();
+        $.swipebox([{
+                href: 'https://www.youtube.com/watch?v=KnAIbNbd1r4&feature=youtu.be',
+                title: 'A sample of aerial shots captured around Charleston'
+            },
+
+
+        ]);
+    });
 
 
     /*---------------- end of jquery ------------------*/
